382c751e7b1a7f37504258befc3d133f4647e81d
Shengping-Zhang <17682319366@163.com>
2019-04-30 15:41:13 +0800

    add factory pattern 结构优化

--- decoratorPattern/src/main/java/com/ping/decoratorpattern/coffee/CondimentDecorator.java
+++ decoratorPattern/src/main/java/com/ping/decoratorpattern/coffee/CondimentDecorator.java
@@ -1,5 +1,9 @@
 package com.ping.decoratorpattern.coffee;
 
+/**
+ * 装饰者基类
+ */
 public abstract class CondimentDecorator extends Beverage {
+    @Override
     public abstract String getDescription();
 }
--- decoratorPattern/src/main/java/com/ping/decoratorpattern/coffee/Mocha.java
+++ decoratorPattern/src/main/java/com/ping/decoratorpattern/coffee/Mocha.java
@@ -7,10 +7,12 @@ public class Mocha extends CondimentDecorator {
         this.beverage = beverage;
     }
 
+    @Override
     public String getDescription() {
         return beverage.getDescription() + ", Mocha";
     }
 
+    @Override
     public double cost() {
         System.out.println("price : "+0.2+ beverage.cost());
         return .20 + beverage.cost();
